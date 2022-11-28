6b86338d23ef68b192db062013387e487f492e69
Phil Alegret <philip.alegret@gmail.com>
2022-11-28 11:58:37 +0000

    Refactored ProductDisplay.vue to use Tailwind.

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -51,12 +51,16 @@ function updateVariant(index) {
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
+  <!-- .product-display -->
+  <div class="p-4">
+    <!-- .product-container -->
+    <div class="flex flex-row flex-wrap">
+      <!-- .product-image -->
+      <div class="w-[100%] md:w-[50%]">
+        <img v-bind:src="image" class="w-[70%] m-10 p-4 border-2 border-solid border-[#d8d8d8]">
       </div>
-      <div class="product-info">
+      <!-- .product-info -->
+      <div class="w-[100%] ml-3 md:w-[50%] md:ml-0">
         <h1>{{ title }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
@@ -69,18 +73,29 @@ function updateVariant(index) {
             {{ detail }}
           </li>
         </ul>
+
+        <!-- 
+        Tailwind code scanning requires background class be static, e.g., 
+        .bg-[green], not dynamically-generated e.g., .bg-[ = green + ].
+        -->
         <div 
           v-for="(variant, index) in variants" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }"
+          class="w-12 h-12 mt-2 border-2 border-solid border-[#d8d8d8] rounded-[50%] cursor-pointer" 
+          :class="{
+            green: 'bg-[green]',
+            blue: 'bg-[blue]',
+          }[variant.color]"
           :key="variant.id" 
           @mouseover="updateVariant(index)" 
         >
         </div>
         
+        <!-- Note use of array in :class binding, instead of string -->
         <button 
-          class="button" 
-          :class="{ disabled: !inStock }" 
+          class="w-40 h-16 m-8 p-5 text-lg leading-none text-white text-center rounded-[5px] btn-shading-bn" 
+          :class="inStock ? 
+            ['bg-[#39495c]', 'cursor-pointer'] : 
+            ['bg-[#d8d8d8]', 'cursor-not-allowed']"
           :disabled="!inStock" 
           @click="addToCart"
         >
@@ -95,69 +110,101 @@ function updateVariant(index) {
 
 .product-display {
   padding: 16px;
+  /* p-4 */
 }
 
 .product-container {
   display: flex;
+  /* flex */
   flex-direction: row;
+  /* flex-row */
   flex-wrap: wrap;
+  /* flex-wrap */
 }
 
 .product-image {
   width: 100%;
+  /* w-full AKA w-[100%] */
 }
 
-img {
+.OBSOLETE img {
   width: 70%;
+  /* w-[70%] */
   margin: 40px;
+  /* m-10 */
   padding: 15px;
+  /* p-4 */
   border: 2px solid #d8d8d8;
+  /* border-2 border-solid border-[#d8d8d8] */
 }
 
 .product-info {
   width: 100%;
+  /* w-full AKA w-[100%] */
   margin-left: 10px;
+  /* ml-3 */
 }
 
 .color-circle {
   width: 50px;
+  /* w-12 */
   height: 50px;
+  /* h-12 */
   margin-top: 8px;
+  /* mt-2 */
   border: 2px solid #d8d8d8;
+  /* border-2 */
   border-radius: 50%;
+  /* rounded-[50%] */
 }
 
 .button {
   width: 160px;
+  /* w-40 */
   height: 60px;
+  /* h-16 */
   margin: 30px;
+  /* m-8 */
   padding: 20px;
+  /* p-5 */
   font-size: 18px;
+  /* text-lg leading-none (to remove added line-height from .text-lg) */
   color: white;
+  /* text-white */
   text-align: center;
+  /* text-center */
   border-radius: 5px;
+  /* rounded-[5px] */
   box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
     inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
     inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
-  
+  /* Moved to index.css as .btn-shading-bn */  
   background-color: #39495c;
+  /* bg-[#39495c] */
   cursor: pointer;
+  /* cursor-pointer */
 }
 
 .button.disabled {
   background-color: #d8d8d8;
+  /*  */
   cursor: not-allowed;
+  /*  */
 }
 
 @media only screen and (min-width: 860px) {
+/* md: --> Breakpoint Modifier */
 
   .product-image {
     width: 50%;
+    /* md:w-6/12 AKA md:w-[50%] */
   }
 
   .product-info {
     width: 50%;
+    /* md:w-6/12 AKA md:w-[50%] */
     margin-left: 0;
+    /* md:ml-0 */
   }
 } 
 </style>
